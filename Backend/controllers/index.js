fc3a1e61cdf6c724530684864c40e89b5f1c3043
Richard Remley <5rremley@gmail.com>
2022-07-21 14:18:09 -0500

    C flat

---
+++ Backend/controllers/index.js
@@ -0,0 +1,41 @@
+const WildAnimal = require("../models/wildAnimals");
+
+const createAnimal = async (req, res) => {
+  try {
+    const wildAnimal = await new WildAnimal(req.body);
+    await wildAnimal.save();
+    return res.status(201).json({
+      wildAnimal,
+    });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
+
+const getAllAnimals = async (req, res) => {
+  try {
+    const wildAnimal = await WildAnimal.find();
+    return res.status(200).json({ wildAnimal });
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
+};
+
+const getAnimalById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const wildAnimal = await WildAnimal.findById(id);
+    if (wildAnimal) {
+      return res.status(200).json({ plant });
+    }
+    return res.status(404).send("Animal with the specified ID does not exists");
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
+};
+
+module.exports = {
+  createAnimal,
+  getAllAnimals,
+  getAnimalById,
+};
