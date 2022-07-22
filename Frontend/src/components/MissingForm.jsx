83660ccc5a905fbd3bafc0b5bf09dfcfb9b0aab6
kennethachan <kennethandrechan@gmail.com>
2022-07-21 21:45:26 -0400

    missingform added

--- Frontend/src/components/MissingForm.jsx
+++ Frontend/src/components/MissingForm.jsx
@@ -3,11 +3,39 @@ const MissingForm = (props) => {
         <form onSubmit={props.getMissingEntities}>
     <input
       type="text"
-      name="search"
       value={props.value}
-      placeholder="Search"
+      placeholder="Name"
       onChange={props.onChange}
     ></input>
+    <br></br>
+    <input
+      type="text"
+      value={props.value}
+      placeholder="Age"
+      onChange={props.onChange}
+    ></input>
+    <br></br>
+    <input
+      type="text"
+      value={props.value}
+      placeholder="Description"
+      onChange={props.onChange}
+    ></input>
+    <br></br>
+       <input
+      type="text"
+      value={props.value}
+      placeholder="Time Missing"
+      onChange={props.onChange}
+    ></input>
+    <br></br>
+    <input
+      type="text"
+      value={props.value}
+      placeholder="Pet or Child"
+      onChange={props.onChange}
+    ></input>
+    <br></br>
      <button className="submit" type="submit"> Submit</button>
         </form>
     )
